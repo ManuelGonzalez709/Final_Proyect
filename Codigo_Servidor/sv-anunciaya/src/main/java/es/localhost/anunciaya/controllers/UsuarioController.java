823df41b8a740e8cd645793e900539dd5316f0fc
ManuelGonzalez709 <manuel5365.mgl@gmail.com>
2024-04-01 19:32:56 +0200

    UserModelCreated

--- Codigo_Servidor/sv-anunciaya/src/main/java/es/localhost/anunciaya/controllers/UsuarioController.java
+++ Codigo_Servidor/sv-anunciaya/src/main/java/es/localhost/anunciaya/controllers/UsuarioController.java
@@ -1,5 +1,56 @@
 package es.localhost.anunciaya.controllers;
 
-public class UsuarioController {
+import java.util.ArrayList;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+		@Autowired
+		private UserController usuarioService;
+	
+		
+		@GetMapping
+		public ArrayList<UserModel> getUsers(){
+			return this.usuarioService.getUsers();
+		}
+		
+		//Create a new user
+		@PostMapping
+		public UserModel saveUser(@RequestBody UserModel user) {
+			return this.usuarioService.saveUser(user);
+		} 
+		
+		@GetMapping(path="/{id}")
+		public Optional<UserModel> getUserById(@PathVariable Long id){
+			return this.usuarioService.getById(id);
+		}
+		
+		@PutMapping(path="/{id}")
+		public UserModel updateById(@RequestBody UserModel request,@PathVariable Long id ) {
+			return this.usuarioService.updateById(request, id);
+			}
+		
+		@DeleteMapping(path="/{id}")	
+		public String deleteById(@PathVariable("id") Long id) {
+			boolean ok = this.usuarioService.deleteUser(id);
+			
+			if(ok) {
+				return "User with id "+id+" deleted";
+			}
+			else {
+				return "Error, it's not possible to deleted user with id "+id;
+			}
+		}
 }
