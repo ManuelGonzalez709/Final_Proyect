0304482fbab5ae6fb9155f807fd3260bf6f67277
Carlos <102172522+carlosmp15@users.noreply.github.com>
2024-04-02 19:48:29 +0200

    Add files via upload

---
+++ sv-springboot/SV-AnunciaYa/src/main/java/es/localhost/anunciaya/SvAnunciaYaApplication.java
@@ -0,0 +1,13 @@
+package es.localhost.anunciaya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvAnunciaYaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SvAnunciaYaApplication.class, args);
+	}
+
+}
