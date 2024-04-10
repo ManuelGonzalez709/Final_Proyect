6752470fb6a2aa89cef9836b875c945d06cc32a7
Carlos <102172522+carlosmp15@users.noreply.github.com>
2024-04-10 17:49:28 +0200

    Delete sv-springboot directory

--- sv-springboot/SV-AnunciaYa/src/main/java/es/localhost/anunciaya/repositories/IPedidoRepository.java
+++
@@ -1,9 +0,0 @@
-package es.localhost.anunciaya.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import es.localhost.anunciaya.models.PedidoModel;
-
-public interface IPedidoRepository extends JpaRepository <PedidoModel, Long>{
-
-}
