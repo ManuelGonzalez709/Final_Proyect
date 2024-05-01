4da6a5a8611f7ae8fcb35f6896c420c5b5ca3fa3
ManuelGonzalez709 <manuel5365.mgl@gmail.com>
2024-05-01 12:40:50 +0200

    NuevoServer
    server

--- sv-php/models/usuario.php
+++ sv-php/models/usuario.php
@@ -3,44 +3,6 @@ require_once 'conexion.php';
 require_once 'util/auth.php';
 
 class Usuario {
-    private $id;
-    private $nombre;
-    private $apellidos;
-    private $telefono;
-    private $email;
-    private $fecha_nac;
-    private $nomb_usu;
-    private $contras;
-
-    public function __construct($id = null, $nombre = null, $apellidos = null, $telefono = null, $email = null, $fecha_nac = null, $nomb_usu = null, $contras = null) {
-        if ($id !== null && $nombre !== null && $apellidos !== null && $telefono !== null && $email !== null && $fecha_nac !== null && $nomb_usu !== null && $contras !== null) {
-            $this->id = $id;
-            $this->nombre = $nombre;
-            $this->apellidos = $apellidos;
-            $this->telefono = $telefono;
-            $this->email = $email;
-            $this->fecha_nac = $fecha_nac;
-            $this->nomb_usu = $nomb_usu;
-            $this->contras = $contras;
-        }
-    }
-
-    public function getId() { return $this->id; }
-    public function getNombre() { return $this->nombre; }
-    public function getApellidos() { return $this->apellidos; }
-    public function getTelefono() { return $this->telefono; }
-    public function getEmail() { return $this->email; }
-    public function getFechaNac() { return $this->fecha_nac; }
-    public function getNombUsu() { return $this->nomb_usu; }
-    public function getContras() { return $this->contras; }
-
-    public function setNombre($nombre) { $this->nombre = $nombre; }
-    public function setApellidos($apellidos) { $this->apellidos = $apellidos; }
-    public function setTelefono($telefono) { $this->telefono = $telefono; }
-    public function setEmail($email) { $this->email = $email; }
-    public function setFechaNac($fecha_nac) { $this->fecha_nac = $fecha_nac; }
-    public function setNombUsu($nomb_usu) { $this->nomb_usu = $nomb_usu; }
-    public function setContras($contras) { $this->contras = $contras; }
     
     /**
      * Función que obtiene todos los datos de un usuario insertado
